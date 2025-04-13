7de65308e8b453b3d33309ce69d8f87508618d07
Yongmin0423 <yym21345@gmail.com>
2025-04-14 00:02:34 +0900

    style: header and search

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Grid from "@mui/material/Grid";
-import Paper from "@mui/material/Paper";
 
+import styles from "./App.module.css";
 import ContactForm from "./components/ContactForm";
 import ContactList from "./components/ContactList";
 
 function App() {
   return (
-    <>
+    <div className={styles.container}>
       <h1>연락처 앱</h1>
       <Grid container spacing={2}>
         <Grid size={6}>
@@ -17,7 +17,7 @@ function App() {
           <ContactList />
         </Grid>
       </Grid>
-    </>
+    </div>
   );
 }
 
